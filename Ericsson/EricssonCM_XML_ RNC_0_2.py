30c4fb08348a4b41b53ba1afd7ecfeacc0157a0a
Gerardo Alfredo Alarcon Rivas <gerardoalfredo2@gmail.com>
2016-09-08 10:46:28 -0500

    Changes in the input directories to testing

--- Ericsson/EricssonCM_XML_ RNC_0_2.py
+++ Ericsson/EricssonCM_XML_ RNC_0_2.py
@@ -354,4 +354,5 @@ def XML_ParserEricsson_CM_RNC(XML_FILE, OutPut_dir):
                                 lines=[]
                                
     print "Done....."                            
-XML_ParserEricsson_CM_RNC('C:\Users\VervebaMX2\Documents\Projects\XML Parsing\TEst\X20160826CM_RNC211.xml','C:\\Users\\VervebaMX2\\Documents\\Projects\\XML Parsing\\TEst\\Result\\')
+XML_ParserEricsson_CM_RNC('C:\Users\VervebaMX2\Documents\Projects\XML Parsing\TEst\X20160826CM_RNC210.xml','C:\\Users\\VervebaMX2\\Documents\\Projects\\XML Parsing\\TEst\\Result\\')
+XML_ParserEricsson_CM_RNC('C:\Users\VervebaMX2\Documents\Projects\XML Parsing\TEst\X20160826CM_RNC220.xml','C:\\Users\\VervebaMX2\\Documents\\Projects\\XML Parsing\\TEst\\Result\\')
